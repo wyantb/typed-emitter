5917d86bce95622ecfcdb582fce16c76c068ac38
Brian Wyant <wyantb@gmail.com>
2016-04-22 20:36:57 -0400

    feat: define core emitter logic and tests

--- emitter.js
+++ emitter.js
@@ -1,6 +1,86 @@
-
+/* eslint no-underscore-dangle: 0 */
 'use strict';
+
+const each = require('lodash/forEach');
 class Emitter {
+    constructor() {
+        this._listeners = {};
+        this._onQueue = [];
+        this._offQueue = [];
+        this._triggerStack = [];
+    }
+
+    on(ctx, ev, fn) {
+        if (!this._listeners[ev]) {
+            this._listeners[ev] = [];
+        }
+
+        const listener = { ctx, ev, fn };
+        if (!this._triggering) {
+            this._listeners[ev].push(listener);
+        } else {
+            this._onQueue.push(listener);
+        }
+    }
+
+    off(ctx) {
+        if (!this._triggering) {
+            each(this._listeners, listeners => {
+                for (let i = 0, len = listeners.length; i < len; i++) {
+                    const listener = listeners[i];
+                    if (listener.ctx === ctx) {
+                        listeners.splice(i, 1);
+                        i--;
+                        len--;
+                    }
+                }
+            });
+        } else {
+            this._offQueue.push(ctx);
+        }
+    }
+
+    trigger(ev) {
+        if (this._triggering) {
+            this._triggerStack.unshift(ev);
+            return;
+        }
+
+        this._triggering = true;
+
+        const type = ev.type;
+        const listeners = this._listeners[type];
+        if (listeners) {
+            for (let i = 0, len = listeners.length; i < len; i++) {
+                const listener = listeners[i];
+                listener.fn.call(listener.ctx, ev);
+            }
+        }
+
+        this._triggering = false;
+        this._processQueue();
+    }
+
+    _processQueue() {
+        if (this._triggering) {
+            return;
+        }
+
+        each(this._offQueue, ctx => {
+            this.off(ctx);
+        });
+        this._offQueue = [];
+
+        each(this._onQueue, listener => {
+            this.on(listener.ctx, listener.ev, listener.fn);
+        });
+        this._onQueue = [];
+
+        if (this._triggerStack.length) {
+            const ev = this._triggerStack.pop();
+            this.trigger(ev);
+        }
+    }
 }
 
 module.exports = Emitter;
--- test.js
+++ test.js
@@ -10,5 +10,37 @@ function assert(bool, msg) {
 }
 
 assert(Emitter, 'a module is exported');
+let em = new Emitter();
+
+// --- Basic tests: on/off/trigger methods
+const callCtx = {};
+let wasCalled = false;
+em.on(callCtx, 'event', function cb() {
+    assert(this === callCtx, 'emitter didnt use given ctx');
+    wasCalled = true;
+});
+assert(!wasCalled, 'was not called yet');
+em.trigger({ type: 'event' });
+assert(wasCalled, 'but was now called');
+wasCalled = false;
+em.off(callCtx);
+assert(!wasCalled, 'why was this called?');
+em.trigger({ type: 'event' });
+assert(!wasCalled, 'after detaching by ctx, trigger wont call it');
+try {
+    em.trigger('thing');
+    assert(false, 'triggering with just string is invalid, didnt throw error');
+} catch (e) {
+    // nothing needed
+}
+
+// --- trigger calls are put onto stack until all listeners are triggered
+// (in this test, if wasn't the case, you'd see inf loop)
+em = new Emitter();
+em.on(callCtx, 'event', () => {
+    em.trigger({ type: 'event' });
+    em.off(callCtx);
+});
+em.trigger('event');
 
 console.log('All tests passed - you\'re good to go');
